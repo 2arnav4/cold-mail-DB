fa60507e1412fe4bdbd33420bf20f8db4cad4bf7
Arnav Singla <singlaarnav2405@gmail.com>
2026-07-14 14:30:08 +0530

    Revert daily_limit to 60 after successful campaign run

--- send_emails.py
+++ send_emails.py
@@ -58,7 +58,7 @@ CONFIG = {
     "db_path": "turso-full.db",
     "template_path": "template.txt",
     "log_path": "sent_log.json",
-    "daily_limit": 100,
+    "daily_limit": 60,
     "tracker_url": _os.environ.get("TRACKER_URL", ""),
 }
 
